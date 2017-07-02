002a2139912404605139140be762e98b124b333c
Huang XU <xuhuang@zucc.edu.cn>
2017-07-02 14:37:22 +0800

    add search

--- src/main/java/com/zucc/demo/controller/EmployeeController.java
+++ src/main/java/com/zucc/demo/controller/EmployeeController.java
@@ -1,24 +1,48 @@
 package com.zucc.demo.controller;
 
+import com.zucc.demo.model.EmployeeVO;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.util.StringUtils;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
 import com.zucc.demo.service.EmployeeManager;
+import org.springframework.web.servlet.ModelAndView;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Controller
 @RequestMapping("/employee-module")
-public class EmployeeController
-{
+public class EmployeeController {
     @Autowired
     EmployeeManager manager;
 
     @RequestMapping(value = "/getAllEmployees", method = RequestMethod.GET)
-    public String getAllEmployees(Model model)
-    {
+    public ModelAndView getAllEmployees(Model model) {
         model.addAttribute("employees", manager.getAllEmployees());
-        return "employeesListDisplay";
+        model.addAttribute("searchWord", new String());
+        return new ModelAndView("employeesListDisplay", "command", new SearchParam());
+    }
+
+
+    @RequestMapping(value = "/searchEmployees", method = RequestMethod.POST)
+    public ModelAndView searchEmployees(SearchParam searchParam, BindingResult br, Model model) {
+        List<EmployeeVO> all = manager.getAllEmployees();
+        if(!StringUtils.isEmpty(searchParam.getWord())) {
+            List<EmployeeVO> result = new ArrayList<>();
+            for(EmployeeVO employeeVO : all) {
+                if(employeeVO.getFirstName().contains(searchParam.getWord())) {
+                    result.add(employeeVO);
+                }
+            }
+            model.addAttribute("employees", result);
+        } else {
+            model.addAttribute("employees", all);
+        }
+        return new ModelAndView("employeesListDisplay", "command", new SearchParam());
     }
 }
--- src/main/webapp/WEB-INF/views/employeesListDisplay.jsp
+++ src/main/webapp/WEB-INF/views/employeesListDisplay.jsp
@@ -7,6 +7,7 @@
 --%>
 <%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core"%>
 <%@ taglib prefix="fmt" uri="http://java.sun.com/jsp/jstl/fmt"%>
+<%@ taglib prefix="form" uri="http://www.springframework.org/tags/form" %>
 
 <html>
 <head>
@@ -15,6 +16,17 @@
 
 <body>
 <h2>All Employees in System</h2>
+
+<form:form action="searchEmployees" method="post">
+    <table>
+        <tr>
+            <td>search word:</td><td><form:input path="word"/></td>
+        </tr>
+        <tr>
+            <td colspan="2"><input type="submit" value="提交"/></td>
+        </tr>
+    </table>
+</form:form>
 
 <table border="1">
     <tr>
@@ -30,8 +42,6 @@
         </tr>
     </c:forEach>
 </table>
-
-
 
 </body>
 </html>
